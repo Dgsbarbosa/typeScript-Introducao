47c25a308018a0ff089abac9f17e8e1bae73bdfb
Dgsbarbosa <guinho.cq@gmail.com>
2022-02-28 11:35:20 -0300

    up

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-function soma(a: number, b: number){
-    return a + b;
+// crianddo uma interface
+
+interface IAnimal {
+    nome: string, // define os parametros para os dados
+    tipo: 'terrestre' | 'aquatico'
 }
-console.log(soma)
+
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean,
+}
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: "terrestre"
+}
+
+const felino: IFelino = {
+    nome: 'Leao',
+    tipo: "terrestre",
+    visaoNoturna: true,
+}
